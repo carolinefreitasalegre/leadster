793929842017c188a2570da292902b198c4bddb1
Carol Freitas <113471098+carolinefreitasalegre@users.noreply.github.com>
2023-07-10 17:10:17 -0400

    Adicionado favicon

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <head></head>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>{children}</body>
     </html>
   );
